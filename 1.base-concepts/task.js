fd6d66c2252ce160a4e21c7bce6b920129d8e1bf
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-06-19 18:32:38 +0300

    Release HomWork Base Concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    return arr;
+} else if (discriminant === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+} else {
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  }
   return arr;
 }
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+    let monthlyPercent = percent / 100 / 12;
+    let loanAmount = amount - contribution;
+    let monthlyPayment = loanAmount * (monthlyPercent / (1 - Math.pow(1 + monthlyPercent, -countMonths)));
+
+    let totalPaid = 0;
+    for (let i = 0; i < countMonths; i++) {
+      totalPaid += monthlyPayment;
+    }
+
+    return Number(totalPaid.toFixed(2));
+  }
