4e0590eca728c801e73b5f0ef6c53c1a83a64598
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-07-09 19:47:19 +0300

    Next2 Release HomeWorkArrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,16 +6,13 @@ function compareArrays(arr1, arr2) {
   }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
   const filteredUsers = users.filter(user => user.gender === gender);
-
-  
   if (filteredUsers.length === 0) {
     return 0;
   }
 
-  
-  const averageAge = filteredUsers.reduce((sum, user) => sum + user.age, 0) / filteredUsers.length;
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / filteredUsers.length;
 
   return averageAge;
 }
