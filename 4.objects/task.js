b88b578c85bc90713b950dd1d95dcc44b5c76bff
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-07-11 17:58:09 +0300

    Release HomeWorkObject

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.subject = null;
+  this.marks = [];
+  this.excluded = null;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (this.excluded) {
+    console.log(`Студент ${this.name} исключен, добавление оценок невозможно.`);
+    return;
+  }
+  this.marks.push(...marksToAdd);
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function(reason) {
+  this.subject = null;
+  this.marks = [];
+  this.excluded = reason;
+  console.log(`Студент ${this.name} был исключен по причине: ${reason}`);
+};
